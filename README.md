eebcbbda242dc06279d6129637d8a81e0e5f222e
Elangoesakkiraj <elangoesakkiraj@gmail.com>
2021-11-29 00:45:01 +0530

    CMake added, Test-bed added #13 * Test cases updated. * c compile with cmake file. * make file modified for cmake v3.5. * ReadMe updated.

--- README.md
+++ README.md
@@ -20,3 +20,28 @@ MQTT Client Using Eclipse Paho Library
 	# for json lib dependency
 
 	sudo apt install libjson-c-dev 
+
+# Build 
+
+	# c files compile with CMake & build the folder structure using (CMAKE & MAKE FILE).
+
+	cd <path>/src/  # staged to this folder 
+
+	mkdir Build     # create directory into the src folder name as build 
+
+	cd build 		# now switch into build folder
+
+	cmake ..		# now type command cmake .. in terminal before check cmake v3.5 is available or not 
+
+	make 			# now cmake generate the more file & dir in build dir. type the command make to compile c lib
+
+	cd ..           # then back to src folder 
+
+	make from_cmake2build # then run this command for create folder structure.
+
+
+	# c file compile & build the folder struct using (GCC cmd & MAKE FILE).
+
+	cd <path>/src   # staged to this folder
+
+	make  			# type make  command to build 
--- bin/mqtt-client-1.sh
+++ bin/mqtt-client-1.sh
@@ -1,1 +1,105 @@
-../lib/client
+echo "SELECT THE TEST PROCESS"
+
+echo "Test 1) will connect"
+echo "Test 2) duplicate connect"
+echo "Test 3) SYS/#  topic"
+echo "Test 4) # topic subscribe"
+echo "Test 5) "
+
+read a
+
+echo "you selected the test case $a"
+
+case $a in 
+	1 ) 
+		echo "########################################################################################################################"
+		echo "will connect..."
+		echo "* ensure in conf file will topic is enable & log level must be 1.give will topic name and will message.then connect the <path/bin/mqtt-client-1.sh client"
+		echo "* connect the another client from <path/bin/mqtt-client-2.sh with subscribe to will topic name."
+		echo "* now disconnect the <path/bin/mqtt-client-1.sh.mqtt-client-1.sh publish will message and mqtt-client-2.sh receive the will message from mqtt-client-1.sh";;
+	2 ) 
+		echo "########################################################################################################################"
+		echo "duplicate connect"
+		echo "* ensure log level must be 3 in conf file.connect the <path/bin/mqtt-client-1.sh client with the clientid <eg: mqtt-client> and subscribe to any topic"
+		echo "* once again connect the <path/bin/mqtt-client-2.sh client with the same clientid <eg: mqtt-client>."
+		echo "* Here,same client id( mqtt-client-2.sh ) doesn't allow to connect with mqtt server at the same time";;
+	3 ) 
+		echo "########################################################################################################################"
+		echo "$SYS/# topic"
+		echo "* log level must be 1 & connect the <path/bin/mqtt-client-1.sh client and subscribe to $SYS/# topic."
+		echo "* it will receive broker performance details and broker metric message rate & network perf from group $SYS/ topic."
+		echo "* we can moniter and build client application.";;
+	4 ) 
+		echo "########################################################################################################################"
+		echo "# topic subscribe"
+		echo "* log level must be 1 & connect the <path/bin/mqtt-client-1.sh client and subscribe to # topic."
+		echo "* it will receives the every messages from broker .whatever message receives broker from all client, include $SYS topic."
+		echo "* it will sents to # subscriber client";;
+	5 ) 
+		echo "";;
+	*) 
+		echo "default"
+esac
+
+control_c() {
+	echo "########################################################################################################################"
+    echo "################################################   TEST RESULT   #######################################################"
+    echo "########################################################################################################################"
+    echo "$a"
+    case $a in
+		1)
+			echo "test case 1:"
+			filecheck=$(cat ../log/client.log | grep -c "topic: will-topic")
+			if [ $filecheck -gt 0 ]; then
+				echo "################################################"
+				echo "will connect sucessfully test pass"
+			else
+				echo "################################################"
+				echo "will connect Test fail"
+			fi
+			echo "Test completed...";;
+    	2)
+			echo "test case 2:"
+			filecheck=$(cat ../log/client.log | grep -c "Failed to connect, return code -1")
+			if [ $filecheck -gt 0 ]; then
+				echo "################################################"
+				echo "duplicate works sucessfully test pass"
+			else
+				echo "################################################ $SYS/broker/version"
+				echo "duplicate Test fail"
+			fi
+			echo "Test completed...";;
+		3)
+			echo "test case 3:"
+			filecheck=$(cat ../log/client.log | grep -c "topic: $SYS/broker/version")
+			if [ $filecheck -gt 0 ]; then
+				echo "################################################"
+				echo "$SYS topic sucessfully test pass"
+			else
+				echo "################################################"
+				echo "$SYS topic Test fail"
+			fi
+			echo "Test completed...";;
+		4)
+			echo "test case 4:"
+			filecheck=$(cat ../log/client.log | grep -c "Message arrived")
+			if [ $filecheck -gt 0 ]; then
+				echo "################################################"
+				echo "# topic subscribe sucessfully test pass"
+			else
+				echo "################################################"
+				echo "# topic subscribe Test fail"
+			fi
+			echo "Test completed...";;
+
+	esac
+
+}
+
+trap control_c INT
+
+../lib/client 
+
+sleep 1
+
+control_c
